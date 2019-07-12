7fa2941b263b44b99e679173ea6da285b52d7ee7
Matttrach <matttrach@gmail.com>
2019-07-12 01:34:37 -0500

    actually creating an empty file

--- Create/create.go
+++ Create/create.go
@@ -1,5 +1,16 @@
 package create
 
+import "os"
+
 func CreateEmptyFile() bool {
-  return true;
+  f, err := os.Create("/mnt/c/Users/mattt/Documents/empty.txt")
+  check(err)
+  f.Close()
+  return true
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
